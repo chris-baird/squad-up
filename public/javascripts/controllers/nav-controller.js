e657eea523c0f8cb5a62f6130aee5c1e9706ae6c
chris baird <cab3953@gmail.com>
2017-03-02 16:16:42 -0800

    thurs filters working

--- public/javascripts/controllers/nav-controller.js
+++ public/javascripts/controllers/nav-controller.js
@@ -1,13 +1,23 @@
 angular.module('app')
 .controller('NavController', NavController);
 
-NavController.$inject = ['$state', 'UserService', 'GameService', 'TokenService'];
+NavController.$inject = ['$timeout', '$state', 'UserService', 'GameService', 'TokenService'];
 
-function NavController($state, UserService, GameService, TokenService) {
+function NavController($timeout, $state, UserService, GameService, TokenService) {
   var vm = this;
+
+  vm.filterMic = 'all';
+  vm.filterSystem = 'all';
 
   vm.getUser = UserService.getUser;
   vm.isLoggedIn = UserService.isLoggedIn;
+
+  GameService.query(function(games) {
+    vm.uniqueSystems = Object.keys(games.reduce((acc, game, idx, arr) => {
+      if (!acc[game.system]) acc[game.system] = true;
+      return acc;
+    }, {}));
+  });
 
   vm.logout = function() {
     // UserService.logout();
@@ -35,5 +45,15 @@ function NavController($state, UserService, GameService, TokenService) {
       window.location.reload();
     });
   };
+
+  vm.filter = function() {
+    return function(game) {
+      if (game) {
+        var mic = vm.filterMic === 'all' || (game.micReq && vm.filterMic === 'yes') || (!game.micReq && vm.filterMic === 'no');
+        var system = vm.filterSystem === 'all' || (game.system === vm.filterSystem);
+        return mic && system;
+      }
+    };
+  }
 
 }

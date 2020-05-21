cb2a4e3ab85f4e0d2ece68c863daeca647ff5ba9
Chris Baird <chrisbaird05@icloud.com>
2020-05-21 14:41:00 -0700

    Changed all the var's in server to const's

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
-var indexRoutes = require('./routes/index');
-var apiRoutes = require('./routes/api');
+const indexRoutes = require('./routes/index');
+const apiRoutes = require('./routes/api');
 
-var app = express();
+const app = express();
 
 require('./config/database');
 
@@ -35,7 +35,7 @@ app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });

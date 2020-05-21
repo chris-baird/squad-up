9d853712c72072753792b33dce5a29ace67d7c3d
Chris Baird <chrisbaird05@icloud.com>
2020-05-21 14:56:16 -0700

    Converted functions to arrow functions

--- server.js
+++ server.js
@@ -34,14 +34,14 @@ app.use('/', indexRoutes);
 app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
